6518571c76776bc01b1eb098f0ad75171a773c28
chrislearn <chris@acroidea.com>
2016-08-05 16:07:15 +0800

    init

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-var fs = require('fs'),
-    path = require('path'),
-    glob = require('globby');
-
-var copy = function(files, destFolder) {
-  files.forEach(function(filename) {
-    var out = fs.createWriteStream(path.join(destFolder, path.basename(filename)));
-    fs.createReadStream(filename).pipe(out);
-  });
-}
-
-var cp = function(pattern, destFolder, cb) {
-  glob(pattern, function(err, files) {
-    copy(files, destFolder);
-    if (cb) cb(err, files);
-  });
-}
-
-var cpSync = function(pattern, destFolder) {
-  copy(glob.sync(pattern), destFolder);
-}
-
-module.exports = {copy: cp, copySync: cpSync};
+module.exports = function(pth, globs) {
+    var providers = require("./index").providers;
+    var pparts = pth.split(/[\\\/]/g);
+    if (typeof globs == "string") {
+        globs = [globs];
+    }
+    var maxMatched = [], matchedGlob;
+    for (var i = 0, il = globs.length; i < il; i++) {
+        var glob = globs[i];
+        if (glob[0] != "!") {
+            var gparts = glob.split(/[\\\/]/g);
+            for (var j = 0, jl = pparts.length; j < jl; j++) {
+                var ppart = pparts[j],
+                    gpart = gparts[j];
+                if (gpart == ppart && j >= maxMatched.length) {
+                    maxMatched.push(ppart);
+                    matchedGlob = glob;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+    pparts.splice(0, maxMatched.length);
+    return {path:pparts.join("/"), glob: matchedGlob};
+};
